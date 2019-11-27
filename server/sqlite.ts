76fe53dd9fbb778e22e26c250ea8372a85670faf
Varuna Jayasiri <vpjayasiri@gmail.com>
2019-11-27 08:58:40 +0530

    read sqlite

---
+++ server/sqlite.ts
@@ -0,0 +1,16 @@
+import * as sqlite3 from "sqlite3"
+let sqlite = sqlite3.verbose()
+let dbPath = '/Users/varuna/ml/lab3/logs/mnist_pytorch/data.sqlite'
+// console.log(sqlite)
+
+let db = new sqlite.Database(dbPath, (err) => {
+    console.log(err)
+})
+
+let sql = `SELECT * FROM scalars`
+
+db.all(sql, [], (err, rows) => {
+    console.log(err)
+    console.log(rows)
+})
+
