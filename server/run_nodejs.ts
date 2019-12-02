3aa77d9ec14796bb3bd3905ba58738f55cd04886
Varuna Jayasiri <vpjayasiri@gmail.com>
2019-12-02 13:57:49 +0530

    🧹  cleanup

--- server/run_nodejs.ts
+++ server/run_nodejs.ts
@@ -90,31 +90,3 @@ export class RunNodeJS {
         return keyValues
     }
 }
-
-// let dbPath = '/Users/varuna/ml/lab3/logs/mnist_pytorch/data.sqlite'
-// // console.log(sqlite)
-
-// let db = new sqlite.Database(dbPath, (err) => {
-//     console.log(err)
-// })
-
-// let sql = `SELECT * FROM scalars`
-
-// db.all(sql, [], (err, rows) => {
-//     // console.log(err)
-//     // console.log(rows)
-//     db.all('SELECT * FROM indicators', [], (err, rows) => {
-//         console.log(err)
-//         console.log(rows)
-//         getCurrentStep()
-//     })
-// })
-
-// /* It will connect to db during query if the DB didn't exist before */
-// function getCurrentStep() {
-//     db.all('SELECT MAX(step) FROM scalars', [], (err, rows) => {
-//         console.log(err)
-//         console.log(rows)
-//         setTimeout(getCurrentStep, 300)
-//     })
-// }
