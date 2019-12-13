ac72b3f872f28eef4c08c377e4adb75bc222394a
Varuna Jayasiri <vpjayasiri@gmail.com>
2019-12-13 09:35:04 +0530

    🐎 scalars in a single table

--- server/run_nodejs.ts
+++ server/run_nodejs.ts
@@ -6,8 +6,6 @@ import * as FS from "fs"
 import * as YAML from "yaml"
 import { EXPERIMENTS_FOLDER } from "./consts"
 
-let sqlite = sqlite3.verbose()
-
 export class RunNodeJS {
     run: Run
     db: sqlite3.Database
@@ -42,15 +40,24 @@ export class RunNodeJS {
     }
 
     private getLastValue(key: string): Promise<any> {
-        let tableName = this.keyToTableName(key)
         return new Promise((resolve, reject) => {
-            this.db.get(`SELECT * FROM ${tableName} ORDER BY step DESC LIMIT 1`, (err, row) => {
-                if(err) {
-                    reject(err)
-                } else {
-                    resolve(row)
-                }
-            })
+            this.db.all(`SELECT a.* FROM scalars AS a
+            INNER JOIN (
+                SELECT indicator, MAX(step) AS step 
+                FROM scalars
+                GROUP BY indicator
+            ) b ON a.indicator = b.indicator AND a.step = b.step`, (err, rows) => {
+               if(err) {
+                   reject(err)
+               } else {
+                    let values = {}
+                    for(let row of rows) {
+                        values[row.indicator] = row
+                    }
+
+                    resolve(values)
+               }
+           })
         })
     }
 
@@ -61,32 +68,20 @@ export class RunNodeJS {
         return new Indicators(YAML.parse(contents))
     }
 
-    private keyToTableName(key: string) {
-        return `values_${key.replace(/\./g, '_')}`
-    }
-
     async getValues() {
         await this.loadDatabase()
         let indicators = await this.getIndicators()
-        let promises = []
-        let keys = []
+        
+        let values = await this.getLastValue('')
+
         for(let k in indicators.indicators) {
             let ind = indicators.indicators[k]
             let key = ind.type == 'scalar' ? ind.name : `${ind.name}.mean`
             if(!ind.options.is_print) {
-                continue
+                delete values[key]
             }
-
-            promises.push(this.getLastValue(key))
-            keys.push(ind.name)
-        }
-        let values = await Promise.all(promises)
-
-        let keyValues = {}
-        for(let i = 0; i < keys.length; ++i) {
-            keyValues[keys[i]] = values[i]
         }
 
-        return keyValues
+        return values
     }
 }
