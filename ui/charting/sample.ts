95cf47472654f08f2ca54fa32e3f3fbb2919b31b
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-07-23 17:40:13 +0530

    axis

--- ui/charting/sample.ts
+++ ui/charting/sample.ts
@@ -1,23 +1,34 @@
 import {ROUTER, SCREEN} from "../app";
 import {ScreenView} from "../screen";
 import {Weya as $, WeyaElement} from '../../lib/weya/weya'
+import {d3} from "../d3"
 
 class ChartView implements ScreenView {
     elem: HTMLElement
 
     render(): WeyaElement {
         let bars = [50, 20, 100, 200, 30]
+        let xScale = d3.scaleLinear().domain([0, 5]).range([0, 250])
+        let axis = d3.axisBottom()
+            .scale(xScale)
+            .ticks(5)
+        let axisElem: SVGGElement
         this.elem = <HTMLElement>$("div.chart", $ => {
-            $('svg', {width: 250, height: 250}, $ => {
-                $('g', {transform: 'translate(2, 248)'}, $ => {
+            $('svg', {width: 252, height: 280}, $ => {
+                $('g', {transform: 'translate(2, 245)'}, $ => {
                     for (let i = 0; i < bars.length; ++i) {
-                        $('g', {transform: `translate(${i * 50}, 0)`}, $ => {
+                        $('g', {transform: `translate(${xScale(i)}, 0)`}, $ => {
                             $('rect', {y: -bars[i], width: 40, height: bars[i], fill: 'red'})
                         })
                     }
                 })
+                axisElem = <SVGGElement>$('g', {transform: 'translate(2, 250)'})
             })
         })
+
+        d3.select(axisElem)
+            .append('g')
+            .call(axis)
 
         return this.elem
     }
---
+++ ui/d3.ts
@@ -0,0 +1,6 @@
+// import * as _CodeMirror from "codemirror";
+
+// let CodeMirror = _CodeMirror
+let d3 = (<any>window).d3
+
+export { d3 }
